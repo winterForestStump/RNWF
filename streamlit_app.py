f7b994c9093c41684e2bd9a0068303f5b3f1dbff
aleks <trikozao@gmail.com>
2024-12-15 21:00:52 +0100

    app

--- streamlit_app.py
+++ streamlit_app.py
@@ -138,7 +138,7 @@ st.altair_chart(structure_chart, use_container_width=True)
 
 "### Main recepients of the Fund"
 recepients = pd.read_csv('https://raw.githubusercontent.com/winterForestStump/RNWF/main/data/recepients.csv', header=0, sep=';')
-st.dataframe(recepients, use_container_width=True)
+st.dataframe(recepients[['Entity', 'RUB, mln']], use_container_width=True)
 
 base = alt.Chart(recepients).encode(
     alt.Theta('RUB, mln:Q').stack(True),
